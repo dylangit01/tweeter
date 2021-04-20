f375dc75d550a560aec4f9d8f24c1de2e9d74d99
Dylan <dcren2010@gmail.com>
2021-04-20 16:37:34 -0400

    implemented charCounter fn by using this

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,11 +2,10 @@ $(document).ready(function() {
 	const textareaEl = $('#tweet-text');
 	
   const charCounter = function () {
-    const outputEl = $('#output');
-    outputEl[0].defaultValue = +140;
-    let inputWordsLength = this.value.length;
-    outputEl[0].defaultValue = 140 - inputWordsLength;
-    outputEl[0].defaultValue < 0 ? outputEl.css('color', '#ff0000') : outputEl.css('color', '#545149');
+		const outputEl = $(this).parent().children().children()[1]
+		let inputWordsLength = this.value.length;
+		outputEl.innerText = 140 - inputWordsLength;
+    outputEl.innerText < 0 ? outputEl.style.color = '#ff0000' : outputEl.style.color = '#545149';
   };
 
   textareaEl.on('keyup', charCounter);
@@ -34,7 +33,7 @@ $(document).ready(function() {
 //   const textareaEl = $('#tweet-text');
 
 //   const charCounter = function () {
-//     const outputEl = $('.counter');
+//     const outputEl = $('#output');
 //     outputEl[0].defaultValue = +140;
 //     let inputWordsLength = this.value.length;
 //     outputEl[0].defaultValue = 140 - inputWordsLength;
