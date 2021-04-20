b1510263cd129f9d3bfe39a3a660459d6aded1ae
Dylan <dcren2010@gmail.com>
2021-04-20 16:09:09 -0400

    created id for output which is more feasible way to do this task

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,30 +1,47 @@
-$(document).ready(function () {
+$(document).ready(function() {
 	const textareaEl = $('#tweet-text');
-	const outputEl = $('.counter')
-	outputEl[0].defaultValue = +140;
-
-	const charCounter = function(e) {
-				console.log(this.value.length);
-		    let inputWordsLength = e.target.value.length;
-        outputEl[0].defaultValue = 140 - inputWordsLength;
-        outputEl[0].defaultValue < 0 ? outputEl.css('color', '#ff0000') : outputEl.css('color', '#545149');
-	}
 	
-	textareaEl.on('keyup', charCounter);
-	
+  const charCounter = function () {
+    const outputEl = $('#output');
+    outputEl[0].defaultValue = +140;
+    let inputWordsLength = this.value.length;
+    outputEl[0].defaultValue = 140 - inputWordsLength;
+    outputEl[0].defaultValue < 0 ? outputEl.css('color', '#ff0000') : outputEl.css('color', '#545149');
+  };
 
-
-
-
-
-
-
-
-
-
-
-
-
+  textareaEl.on('keyup', charCounter);
 
   // end
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// My version:
+
+// $(document).ready(function () {
+//   const textareaEl = $('#tweet-text');
+
+//   const charCounter = function () {
+//     const outputEl = $('.counter');
+//     outputEl[0].defaultValue = +140;
+//     let inputWordsLength = this.value.length;
+//     outputEl[0].defaultValue = 140 - inputWordsLength;
+//     outputEl[0].defaultValue < 0 ? outputEl.css('color', '#ff0000') : outputEl.css('color', '#545149');
+//   };
+
+//   textareaEl.on('keyup', charCounter);
+
+//   // end
+// });
