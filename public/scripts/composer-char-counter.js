d6be957a3772569137007a62ebeceb74b4dc55dd
Dylan <dcren2010@gmail.com>
2021-04-20 14:53:02 -0400

    used this to test function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,15 @@ $(document).ready(function () {
 	const textareaEl = $('#tweet-text');
 	const outputEl = $('.counter')
 	outputEl[0].defaultValue = +140;
+
+	const charCounter = function(e) {
+				console.log(this.value.length);
+		    let inputWordsLength = e.target.value.length;
+        outputEl[0].defaultValue = 140 - inputWordsLength;
+        outputEl[0].defaultValue < 0 ? outputEl.css('color', '#ff0000') : outputEl.css('color', '#545149');
+	}
 	
-	textareaEl.on('keyup', function (e) {
-    let inputWordsLength = e.target.value.length;
-    outputEl[0].defaultValue = 140 - inputWordsLength;
-		outputEl[0].defaultValue < 0 ? outputEl.css('color', '#ff0000') : outputEl.css('color', '#545149');
-  });
+	textareaEl.on('keyup', charCounter);
 	
 
 
