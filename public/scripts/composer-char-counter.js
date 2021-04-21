ce5c11269da9bd419b408b65b86d844a5e8a1a41
Dylan <dcren2010@gmail.com>
2021-04-20 20:42:40 -0400

    update char-counter negtive number using jquery

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,18 @@
-$(document).ready(function() {
-	const textareaEl = $('#tweet-text');
-	
+$(document).ready(function () {
+  const textareaEl = $('#tweet-text');
+
   const charCounter = function () {
-		const outputEl = $(this).parent().children().children()[1]
-		outputEl.innerText = 140 - this.value.length;
-    outputEl.innerText < 0 ? outputEl.style.color = '#ff0000' : outputEl.style.color = '#545149';
+    const outputEl = $(this).parent().children().children()[1];
+    outputEl.innerText = 140 - this.value.length;
+    // outputEl.innerText < 0 ? outputEl.style.color = '#ff0000' : outputEl.style.color = '#545149';
+    outputEl.innerText < 0 ? $(outputEl).css('color', '#ff0000') : $(outputEl).css('color', '#545149');
   };
 
   textareaEl.on('keyup', charCounter);
 
   // end
 });
+
 
 
 
