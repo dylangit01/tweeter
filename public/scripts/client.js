39624f5bee7b064f80a6ecdbc170dd31b9caf227
Dylan <dcren2010@gmail.com>
2021-04-21 15:34:10 -0400

    Fetching tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    user: {
-      name: 'Newton',
-      avatars: 'https://i.imgur.com/73hZDYK.png',
-      handle: '@SirIsaac',
-    },
-    content: {
-      text: 'If I have seen further it is by standing on the shoulders of giants',
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: 'Descartes',
-      avatars: 'https://i.imgur.com/nlhLi3I.png',
-      handle: '@rd',
-    },
-    content: {
-      text: 'Je pense , donc je suis',
-    },
-    created_at: 1461113959088,
-  },
-];
-
-$('document').ready(function () {
+$('document').ready(function() {
+  
   const renderTweets = function (tweets) {
     tweets.forEach((tweet) => {
       const $tweet = createTweetElement(tweet);
@@ -60,7 +36,7 @@ $('document').ready(function () {
 	`);
   };
 
-  renderTweets(data);
+  // renderTweets(data);
 
   // Below fn cannot use "this", otherwise use tradition function syntax.
   $('.new-tweet form').submit((event) => {
@@ -71,11 +47,18 @@ $('document').ready(function () {
       type: 'POST',
       url: '/tweets',
       data: $(event.target).serialize(),
-    }).then(function () {
+    }).then(() => {
       console.log('Successfully loaded');
     });
   });
 
+  const loadTweets = () => {
+    $.ajax({ url: '/tweets' }).then(data => {
+      renderTweets(data)
+    })
+  }
+
+  loadTweets()
 
 
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -9,3 +9,4 @@ $('document').ready(function () {
 
   textareaEl.on('keyup', charCounter);
 });
+
