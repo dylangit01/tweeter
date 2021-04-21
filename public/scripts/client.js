e50fd52357e6cbc153a786fca532a5186c80649f
Dylan <dcren2010@gmail.com>
2021-04-21 07:47:15 -0400

    Implemented dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,31 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    user: {
+      name: 'Newton',
+      avatars: 'https://i.imgur.com/73hZDYK.png',
+      handle: '@SirIsaac',
+    },
+    content: {
+      text: 'If I have seen further it is by standing on the shoulders of giants',
+    },
+    created_at: 1461116232227,
+  },
+  {
+    user: {
+      name: 'Descartes',
+      avatars: 'https://i.imgur.com/nlhLi3I.png',
+      handle: '@rd',
+    },
+    content: {
+      text: 'Je pense , donc je suis',
+    },
+    created_at: 1461113959088,
+  },
+];
+
 const tweetData = {
   user: {
     name: 'Newton',
@@ -16,33 +41,35 @@ const tweetData = {
   created_at: 1461116232227,
 };
 
+const renderTweets = function (tweets) {
+	tweets.forEach(tweet => {
+		const $tweet = createTweetElement(tweet);
+		$('#tweets-container').append($tweet);
+	})
+};
+
 const createTweetElement = function (data) {
-  const {
-    user: { name, avatars, handle },
-    content: { text },
-    created_at,
-  } = data;
-  return $(`
-		<header>
-			<div class="faceName">
-				<span><img src="${avatars}" alt="userAvatar"/></i>${name}</span>
-				<span>${handle}</span>
-			</div>
-			<p>${text}</p>
-		</header>
-		<footer>
-      <span class="timePassed" datetime="${created_at}"></span>
-      <span>
-        <i class="fas fa-flag fa-xs"></i>
-        <i class="fas fa-retweet fa-xs"></i>
-        <i class="fas fa-heart fa-xs"></i>
-      </span>
-    </footer>
+  const { user: { name, avatars, handle }, content: { text }, created_at, } = data;
+	return $(`
+		<article class='tweet'>
+			<header>
+				<div class="faceName">
+					<span><img src="${avatars}" alt="userAvatar"/></i>${name}</span>
+					<span>${handle}</span>
+				</div>
+				<p>${text}</p>
+			</header>
+			<footer>
+				<span class="timePassed" datetime="${created_at}"></span>
+				<span>
+					<i class="fas fa-flag fa-xs"></i>
+					<i class="fas fa-retweet fa-xs"></i>
+					<i class="fas fa-heart fa-xs"></i>
+				</span>
+			</footer>
+		</article>
 	`);
 };
 
-const $tweet = createTweetElement(tweetData);
+renderTweets(data);
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('.tweet').append($tweet);
