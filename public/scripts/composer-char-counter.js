8c4f319eea67653f2825b90e23d9785fc838793e
Dylan <dcren2010@gmail.com>
2021-04-25 13:44:57 -0400

    change code logic per eval feedback

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,12 @@
-$('document').ready(function () {
+$('document').ready(function() {
   const textareaEl = $('#tweet-text');
 
   const charCounter = function () {
     const outputEl = this.parentElement.querySelector('output');
     outputEl.innerText = 140 - this.value.length;
-    outputEl.innerText < 0 ? $(outputEl).css('color', '#ff0000') : $(outputEl).css('color', '#545149');
+    // outputEl.innerText < 0 ? $(outputEl).css('color', '#ff0000') : $(outputEl).css('color', '#545149');
+    // outputEl.innerText < 0 ? outputEl.classList.add('negNumColor') : outputEl.classList.remove('negNumColor')
+    outputEl.innerText < 0 ? $(outputEl).addClass('negNumColor') : $(outputEl).removeClass('negNumColor');
   };
   
   textareaEl.on('keyup', charCounter);
