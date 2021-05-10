4a927b4fa1425f6f6c3f05c3e88f52a233168ad8
Dylan <dcren2010@gmail.com>
2021-05-10 08:13:43 -0400

    add jquery usage in composer-counter.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,20 @@
-$('document').ready(function() {
+$(document).ready(function() {
   const textareaEl = $('#tweet-text');
 
   const charCounter = function () {
-    const outputEl = this.parentElement.querySelector('output');
-    outputEl.innerText = 140 - this.value.length;
-    // outputEl.innerText < 0 ? $(outputEl).css('color', '#ff0000') : $(outputEl).css('color', '#545149');
-    // outputEl.innerText < 0 ? outputEl.classList.add('negNumColor') : outputEl.classList.remove('negNumColor')
-    outputEl.innerText < 0 ? $(outputEl).addClass('negNumColor') : $(outputEl).removeClass('negNumColor');
+    const $testEl = $(this).closest('form');    //closest is used to find the parent element of this event
+    const $formEl = $(this).parent();
+    console.log($formEl);
+
+    // const $outputEl = $(this).next().find('output');
+    const $outputEl = $(this).parent().find('output')
+    $outputEl.text(140 - this.value.length)
+    $outputEl.text() < 0 ? $outputEl.addClass('negNumColor') : $outputEl.removeClass('negNumColor');
+
+    // const outputEl = this.parentElement.querySelector('output');
+    // outputEl.innerText = 140 - this.value.length;
+    // outputEl.innerText < 0 ? $(outputEl).css('color', '#ff0000') : $(outputEl).css('color', '#545149');          //inline css
+    // outputEl.innerText < 0 ? outputEl.classList.add('negNumColor') : outputEl.classList.remove('negNumColor')    //DOM add/remove css
   };
   
   textareaEl.on('keyup', charCounter);
